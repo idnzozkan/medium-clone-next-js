ca6fe873b93ec783a2af4e58811513e3309e73d9
Deniz Ozkan <ibrahimdenizozkan@gmail.com>
2022-03-21 22:23:03 +0300

    Use individual post items on the posts component

--- next/components/Posts.tsx
+++ next/components/Posts.tsx
@@ -1,5 +1,19 @@
-const Posts = () => {
-  return <div>Posts</div>
+import { FunctionComponent } from 'react'
+import { PostType } from '../typings'
+import PostItem from './PostItem'
+
+interface Props {
+  posts: [PostType]
+}
+
+const Posts: FunctionComponent<Props> = ({ posts }) => {
+  return (
+    <div className="md:pd-6 m-auto my-5 grid max-w-6xl grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 lg:grid-cols-3">
+      {posts.map((post) => (
+        <PostItem key={post._id} post={post} />
+      ))}
+    </div>
+  )
 }
 
 export default Posts
